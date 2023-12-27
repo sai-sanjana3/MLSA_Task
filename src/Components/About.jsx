35c547f2a99b85634c1c9fb224f73aa84a8dc1eb
sai-sanjana3 <102915803+sai-sanjana3@users.noreply.github.com>
2023-12-27 22:08:14 +0530

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "simple background image";
 
 /**
  * Sort description that expands on your title on the Home component.
@@ -31,11 +31,11 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Web development",
+  "Networking",
+  "Machine Learning",
+  "Cloud Computing",
+  "Data bases",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am inquisitive computer science engineer who have good knowledge in math, logic and development. I am interested to solve real world world problems and make people's lives easier with changing technology and world in an optimal way.";
 
 const About = () => {
   return (
